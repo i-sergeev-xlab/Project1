3baaeb8b32f5c58f28ea25191191b281895ef0ae
i-sergeev-xlab <bpatkavanek@mail.ru>
2024-12-25 14:57:52 +0300

    unit-2-all

---
+++ Assets/2-1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b;
+            b = 4;
+
+            byte b;
+            sbyte sb;
+            short s;
+            ushort us;
+            int i;
+            uint i;
+            long l;
+            ulong ul;
+
+            float f = 6.2f;
+            double d = 5.7;
+
+            char c = '5';
+
+            string str = "Hello World";
+
+            bool bl;
+
+
+
+        }
+    }
+}
---
+++ Assets/2-2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        class House
+        {
+            public string street;
+            public int number;
+
+            public void Print()
+            { 
+                Console.WriteLine($"{street}, {number}");
+            }       
+        }
+        static void Main(string[] args)
+        {
+            House Tower = new House();
+            Tower.street = "Красная";
+            Tower.number = 50;
+
+            Tower.Print();
+        }
+
+    }
+}
